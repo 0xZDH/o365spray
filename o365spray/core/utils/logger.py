fc8f349eae47298ec7bfe16f07693771d866a19f
0xZDH <11055684+0xZDH@users.noreply.github.com>
2022-11-13 01:10:12 -0500

    Update logging output

--- o365spray/core/utils/logger.py
+++ o365spray/core/utils/logger.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python3
+# fmt: off
 
 import logging
 
 from o365spray.core.utils.colors import text_colors as bcolors
 
 
-# fmt: off
-
-
 class LoggingLevels:
-    CRITICAL = f"{bcolors.FAIL}%s{bcolors.ENDC}" % "crit"
-    WARNING  = f"{bcolors.WARNING}%s{bcolors.ENDC}" % "warn"
-    DEBUG    = f"{bcolors.OKBLUE}%s{bcolors.ENDC}" % "debg"
-    ERROR    = f"{bcolors.FAIL}%s{bcolors.ENDC}" % "fail"
-    INFO     = f"{bcolors.OKGREEN}%s{bcolors.ENDC}" % "info"
+    CRITICAL = f"{bcolors.FAIL}%s{bcolors.ENDC}" % "crit"     # 50
+    ERROR    = f"{bcolors.FAIL}%s{bcolors.ENDC}" % "fail"     # 40
+    WARNING  = f"{bcolors.WARNING}%s{bcolors.ENDC}" % "warn"  # 30
+    INFO     = f"{bcolors.OKBLUE}%s{bcolors.ENDC}" % "info"   # 20
+    DEBUG    = f"{bcolors.OKBLUE}%s{bcolors.ENDC}" % "debg"   # 10
 
 
 def init_logger(debug: bool):
-    """Initialize program logging"""
+    """Initialize program logging
+
+    Arguments:
+        debug: if debugging is enabled
+    """
     if debug:
         logging_level = logging.DEBUG
-        logging_format = ("[%(asctime)s] %(levelname)-5s | %(filename)17s:%(lineno)-4s | %(message)s")
+        logging_format = "[%(asctime)s] %(levelname)-5s | %(filename)17s:%(lineno)-4s | %(message)s"
     else:
         logging_level = logging.INFO
         logging_format = "[%(asctime)s] %(levelname)-5s | %(message)s"
 
     logging.basicConfig(format=logging_format, level=logging_level)
 
-    # Handle color output
-    logging.addLevelName(logging.CRITICAL, LoggingLevels.CRITICAL)
-    logging.addLevelName(logging.WARNING,  LoggingLevels.WARNING)
-    logging.addLevelName(logging.DEBUG,    LoggingLevels.DEBUG)
-    logging.addLevelName(logging.ERROR,    LoggingLevels.ERROR)
-    logging.addLevelName(logging.INFO,     LoggingLevels.INFO)
+    # Update log level names with colorized output
+    logging.addLevelName(logging.CRITICAL, LoggingLevels.CRITICAL)  # 50
+    logging.addLevelName(logging.ERROR,    LoggingLevels.ERROR)     # 40
+    logging.addLevelName(logging.WARNING,  LoggingLevels.WARNING)   # 30
+    logging.addLevelName(logging.INFO,     LoggingLevels.INFO)      # 20
+    logging.addLevelName(logging.DEBUG,    LoggingLevels.DEBUG)     # 10
